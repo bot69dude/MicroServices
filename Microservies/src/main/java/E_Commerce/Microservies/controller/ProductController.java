452ab935540aa89b3e73924f330dc1ef7f363a88
bot69dude <nrithvik19@gmail.com>
2024-08-17 11:48:31 +0530

    added Get request

--- Microservies/src/main/java/E_Commerce/Microservies/controller/ProductController.java
+++ Microservies/src/main/java/E_Commerce/Microservies/controller/ProductController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import E_Commerce.Microservies.dto.ProductRequest;
+import E_Commerce.Microservies.dto.ProductResponse;
 import E_Commerce.Microservies.service.ProductService;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -25,6 +29,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 
 }
